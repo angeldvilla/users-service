dcf0c15d5d9dc4733d5e9fff4ea191475d4a550c
angeldvilla <escarlata.2003@gmail.com>
2024-09-16 00:10:06 -0500

    Add sync Login table in database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const sequelize = require('./config/db');
 const Users = require('./models/users');
 const Roles = require('./models/roles');
+const Status = require('./models/status');
+const Login = require('./models/login');
 
 sequelize.sync({ force: true })
     .then(() => {
