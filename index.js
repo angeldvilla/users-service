1b04a1f63c3fdb020e48b26049baf431ffe20d77
angeldvilla <escarlata.2003@gmail.com>
2024-09-16 00:08:55 -0500

    Add routes for login service

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ const sequelize = require('./src/config/db');
 const userService = require('./src/service/userService');
 const rolesService = require('./src/service/roleService');
 const statusService = require('./src/service/statService');
+const loginService = require('./src/service/loginService');
 
 //WSDL
 const usersWSDL = fs.readFileSync('src/wsdl/users.wsdl', 'utf8');
 const rolesWSDL = fs.readFileSync('src/wsdl/roles.wsdl', 'utf8');
 const statusesWSDL = fs.readFileSync('src/wsdl/status.wsdl', 'utf8');
+const loginWSDL = fs.readFileSync('src/wsdl/login.wsdl', 'utf8');
 
 const app = express();
 const port = 3002;
@@ -39,97 +41,120 @@ sequelize.authenticate()
     .then(() => {
         console.log('La conexión se ha establecido correctamente.');
 
-        // Página de presentación usuarios
-        app.get('/users', (req, res) => {
+        // rutas
+        app.get("/users", (req, res) => {
             res.send(`
-            <html>
-                <head>
-                <title>Servicio de Usuarios</title>
-                </head>
-                <body>
-                <h1>Bienvenido al Servicio de Usuarios</h1>
-                <p>Este servicio SOAP permite realizar las siguientes operaciones:</p>
-                <ul>
-                    <li>Obtener todos los usuarios: <code>getAllUsers</code></li>
-                    <li>Obtener detalles de un usuario: <code>getUserDetail</code></li>
-                    <li>Crear un usuario: <code>createUser</code></li>
-                    <li>Actualizar un usuario: <code>updateUser</code></li>
-                    <li>Eliminar un usuario: <code>deleteUser</code></li>
-                </ul>
-                <p>Puedes ver el archivo WSDL desde el siguiente enlace:</p>
-                <a href="/users/wsdl">Ver WSDL</a>
-                </body>
-            </html>
-            `);
+        <html>
+            <head>
+            <title>Servicio de Usuarios</title>
+            </head>
+            <body>
+            <h1>Bienvenido al Servicio de Usuarios</h1>
+            <p>Este servicio SOAP permite realizar las siguientes operaciones:</p>
+            <ul>
+                <li>Obtener todos los usuarios: <code>getAllUsers</code></li>
+                <li>Obtener detalles de un usuario: <code>getUserDetail</code></li>
+                <li>Crear un usuario: <code>createUser</code></li>
+                <li>Actualizar un usuario: <code>updateUser</code></li>
+                <li>Eliminar un usuario: <code>deleteUser</code></li>
+            </ul>
+            <p>Puedes ver el archivo WSDL desde el siguiente enlace:</p>
+            <a href="/users/wsdl">Ver WSDL</a>
+            </body>
+        </html>
+        `);
+        }
+        );
+
+        app.get("/roles", (req, res) => {
+            res.send(`
+    <html>
+        <head>
+        <title>Servicio de Roles</title>
+        </head>
+        <body>
+        <h1>Bienvenido al Servicio de Roles</h1>
+        <p>Este servicio SOAP permite realizar las siguientes operaciones:</p>
+        <ul>
+            <li>Obtener todos los roles: <code>getRoles</code></li>
+            <li>Crear un rol: <code>createRole</code></li>
+        </ul>
+        <p>Puedes ver el archivo WSDL desde el siguiente enlace:</p>
+        <a href="/roles/wsdl">Ver WSDL</a>
+        </body>
+    </html>
+    `);
         });
 
-        // Página de presentación estados
-        app.get('/statuses', (req, res) => {
+        app.get("/statuses", (req, res) => {
             res.send(`
-            <html>
-                <head>
-                <title>Servicio de Estados</title>
-                </head>
-                <body>
-                <h1>Bienvenido al Servicio de Estados</h1>
-                <p>Este servicio SOAP permite realizar las siguientes operaciones:</p>
-                <ul>
-                    <li>Obtener todos los estados: <code>getStatus</code></li>
-                    <li>Crear un estado: <code>createStatus</code></li>
-                </ul>
-                <p>Puedes ver el archivo WSDL desde el siguiente enlace:</p>
-                <a href="/statuses/wsdl">Ver WSDL</a>
-                </body>
-            </html>
-            `);
+    <html>
+        <head>
+        <title>Servicio de Estados</title>
+        </head>
+        <body>
+        <h1>Bienvenido al Servicio de Estados</h1>
+        <p>Este servicio SOAP permite realizar las siguientes operaciones:</p>
+        <ul>
+            <li>Obtener todos los estados: <code>getStatus</code></li>
+            <li>Crear un estado: <code>createStatus</code></li>
+        </ul>
+        <p>Puedes ver el archivo WSDL desde el siguiente enlace:</p>
+        <a href="/statuses/wsdl">Ver WSDL</a>
+        </body>
+    </html>
+    `);
         });
 
-        // Página de presentación roles
-        app.get('/roles', (req, res) => {
+        app.get("/login", (req, res) => {
             res.send(`
-            <html>
-                <head>
-                <title>Servicio de Roles</title>
-                </head>
-                <body>
-                <h1>Bienvenido al Servicio de Roles</h1>
-                <p>Este servicio SOAP permite realizar las siguientes operaciones:</p>
-                <ul>
-                    <li>Obtener todos los roles: <code>getRoles</code></li>
-                    <li>Crear un rol: <code>createRole</code></li>
-                </ul>
-                <p>Puedes ver el archivo WSDL desde el siguiente enlace:</p>
-                <a href="/roles/wsdl">Ver WSDL</a>
-                </body>
-            </html>
-            `);
+    <html>
+        <head>
+        <title>Servicio de Login</title>
+        </head>
+        <body>
+        <h1>Bienvenido al Servicio de Login</h1>
+        <p>Este servicio SOAP permite realizar las siguientes operaciones:</p>
+        <ul>
+            <li>Iniciar Sesión: <code>login</code></li>
+            <li>Cerrar Sesión: <code>logout</code></li>
+        </ul>
+        <p>Puedes ver el archivo WSDL desde el siguiente enlace:</p>
+        <a href="/login/wsdl">Ver WSDL</a>
+        </body>
+    </html>
+    `);
         });
 
-        // Servir el archivo WSDL
+        // Servir archivos WSDL
         app.get('/statuses/wsdl', (req, res) => {
             res.set('Content-Type', 'text/xml');
             res.send(statusesWSDL);
         });
 
-        // Servir el archivo WSDL
         app.get('/roles/wsdl', (req, res) => {
             res.set('Content-Type', 'text/xml');
             res.send(rolesWSDL);
         });
 
-        // Servir el archivo WSDL
         app.get('/users/wsdl', (req, res) => {
             res.set('Content-Type', 'text/xml');
             res.send(usersWSDL);
+        });
+
+        app.get('/login/wsdl', (req, res) => {
+            res.set('Content-Type', 'text/xml');
+            res.send(loginWSDL);
         });
 
         app.listen(port, function () {
             soap.listen(app, '/users', userService, usersWSDL);
             soap.listen(app, '/roles', rolesService, rolesWSDL);
             soap.listen(app, '/statuses', statusService, statusesWSDL);
+            soap.listen(app, '/login', loginService, loginWSDL);
             console.log(`Servidor SOAP corriendo en el puerto ${port}`);
         });
     })
     .catch(err => {
-        console.error('No se puede conectar a la base de datos:', err);
+        console.log('No se puede conectar a la base de datos:', err);
     });
