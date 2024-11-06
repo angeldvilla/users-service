ff4fa6bee71885def142dfa0d22a2049a84baed7
angeldvilla <escarlata.2003@gmail.com>
2024-11-06 12:46:11 -0500

    update backup db

--- users_db.sql
+++ users_db.sql
@@ -2,7 +2,7 @@
 -- Host:                         127.0.0.1
 -- Versión del servidor:         10.4.32-MariaDB - mariadb.org binary distribution
 -- SO del servidor:              Win64
--- HeidiSQL Versión:             12.6.0.6765
+-- HeidiSQL Versión:             12.5.0.6677
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -14,10 +14,15 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Volcando estructura de base de datos para users_db
+CREATE DATABASE IF NOT EXISTS `users_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `users_db`;
+
 -- Volcando estructura para tabla users_db.logins
 CREATE TABLE IF NOT EXISTS `logins` (
   `id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `token` varchar(255) NOT NULL,
+  `token` varchar(255) DEFAULT NULL,
   `user_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `isActive` tinyint(1) NOT NULL DEFAULT 0,
   `createdAt` datetime NOT NULL,
@@ -29,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `logins` (
 
 -- Volcando datos para la tabla users_db.logins: ~1 rows (aproximadamente)
 INSERT IGNORE INTO `logins` (`id`, `token`, `user_id`, `isActive`, `createdAt`, `updatedAt`) VALUES
-	('8dc353ee-791a-4264-98e2-a3fc26f24e39', 'v2uwqfzr7s', 'b3a4ef56-c030-462d-842a-c67aa17f4c57', 0, '2024-09-18 02:00:31', '2024-09-18 02:05:46');
+	('f5a59002-7954-4474-89fa-7bafcbd24867', '', '7cea2b3f-525a-4f62-8ca4-3bf59d95e366', 0, '2024-11-06 16:25:19', '2024-11-06 17:44:48');
 
 -- Volcando estructura para tabla users_db.roles
 CREATE TABLE IF NOT EXISTS `roles` (
@@ -38,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `roles` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla users_db.roles: ~3 rows (aproximadamente)
+-- Volcando datos para la tabla users_db.roles: ~2 rows (aproximadamente)
 INSERT IGNORE INTO `roles` (`id`, `rol`) VALUES
 	(1, 'Administrador'),
 	(2, 'Empleado'),
@@ -51,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `statuses` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla users_db.statuses: ~3 rows (aproximadamente)
+-- Volcando datos para la tabla users_db.statuses: ~2 rows (aproximadamente)
 INSERT IGNORE INTO `statuses` (`id`, `status_name`) VALUES
 	(1, 'Activo'),
 	(2, 'Inactivo'),
@@ -79,13 +84,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   CONSTRAINT `users_ibfk_2` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla users_db.users: ~5 rows (aproximadamente)
+-- Volcando datos para la tabla users_db.users: ~4 rows (aproximadamente)
 INSERT IGNORE INTO `users` (`id`, `name`, `lastname`, `address`, `phone`, `email`, `username`, `password`, `role_id`, `status_id`) VALUES
-	('1ea16e5f-dd43-4be4-9db4-cf87111051ac', 'Jane', 'Smith', '456 Oak Avenue, Riverside', '555-5678', 'janesmith@example.com', 'janesmith', '$2b$10$YJRvS1Ii30fbj5QsPtu7R.lmj9uzT7fZBXc1GVjsVWPSdR/m6AFQm', 3, 1),
-	('40336a14-2451-471c-8863-f64626186664', 'Michael', 'Johnson', '789 Pine Road, Lakeview', '555-9012', 'michaelj@example.com', 'michaelj', '$2b$10$AkRZBl3iJhM2zJy7Av5QVO6h77zojYOAf4jY0nFChb9gObxdIdDgy', 3, 1),
-	('b3a4ef56-c030-462d-842a-c67aa17f4c57', 'John', 'Doe', '123 Maple Street, Springfield', '555-1234', 'johndoe@example.com', 'johndoe', '$2b$10$Bl0xl4hDRSOL1jOiKx3lWeRm4vnGD5tHak11PekYwxhZ7DJIBFflu', 1, 1),
-	('ce9967ed-fc21-46d5-9cc7-fbcf4fc05c5d', 'David', 'Martinez', '202 Birch Boulevard, Hillside', '555-7890', 'davidm@example.com', 'davidm', '$2b$10$qf9lmRBkr/bH7lAPW0IWYeTuWks4qmL1x5BLWZjs5bGYsFrSfXauu', 2, 1),
-	('ea34aaab-30b0-4281-8081-21d02e7b1e88', 'Emily', 'Davis', '101 Cedar Lane, Brookfield', '555-3456', 'emilydavis@example.com', 'emilydavis', '$2b$10$tEMkHhwoDnzphzaWIM7zgOp8Vb9g4La4G9Fha2BCKWFUYhXUY3zya', 2, 1);
+	('606065aa-4f7a-48fd-bb16-8b86976423b7', 'Michael', 'Johnson', '789 Pine Road, Lakeview', '555-9012', 'michaelj@example.com', 'michaelj', '$2b$10$K/SMtFhT5k0AeCoN6Gij3.pcsHU9/C0vZuYcDC4W/ejcihG35Gmfe', 3, 1),
+	('7cea2b3f-525a-4f62-8ca4-3bf59d95e366', 'John', 'Doe', '123 Maple Street, Springfield', '555-1234', 'johndoe@example.com', 'johndoe', '$2b$10$HFDP1lquzZTtJWjTsUoE6uvch1LYAuPV3sf3FNrzDzMpTO6fbh112', 1, 1),
+	('b32687a7-f041-496f-ac6d-01a975728f8f', 'Emily', 'Davis', '101 Cedar Lane, Brookfield', '555-3456', 'emilydavis@example.com', 'emilydavis', '$2b$10$uAyGp3POgu9OTdXWPf.h8ec3cLRFajtw8WA9rsfZmkC7PkE/zhcxu', 2, 1),
+	('ced51417-496b-4893-b553-c7c0de4fc8e7', 'Jane', 'Smith', '456 Oak Avenue, Riverside', '555-5678', 'janesmith@example.com', 'janesmith', '$2b$10$lmFlbuJ0YxnnpIPrTXwWkejY9PWb9TK4tkUuEbeKtzj0Noaw95dcq', 3, 1),
+	('f80c2d9a-8bee-46a8-ab22-5bb495f6531c', 'David', 'Martinez', '202 Birch Boulevard, Hillside', '555-7890', 'davidm@example.com', 'davidm', '$2b$10$7eDfayA.o5Iq9W9t9aATMuxZBpc48tnqbKJenVK03uTHSCuFNReha', 2, 1);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
