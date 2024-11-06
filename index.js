0fc61b1ac0549ca3aedda80d4a84351535f67487
angeldvilla <escarlata.2003@gmail.com>
2024-11-06 11:45:43 -0500

    update clean structure for users service

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const sequelize = require('./src/config/db');
 
+//rutas
+const routes = require('./src/routes/index');
+
 //servicios
 const userService = require('./src/service/userService');
 const rolesService = require('./src/service/roleService');
 const statusService = require('./src/service/statService');
 const loginService = require('./src/service/loginService');
-const { viewProductsClient, searchProduct, filterProducts } = require('./src/service/viewProducts');
 
 //WSDL
 const usersWSDL = fs.readFileSync('src/wsdl/users.wsdl', 'utf8');
@@ -43,145 +45,7 @@ sequelize.authenticate()
         console.log('La conexión se ha establecido correctamente.');
 
         // rutas
-        app.get("/users", (req, res) => {
-            res.send(`
-        <html>
-            <head>
-            <title>Servicio de Usuarios</title>
-            </head>
-            <body>
-            <h1>Bienvenido al Servicio de Usuarios</h1>
-            <p>Este servicio SOAP permite realizar las siguientes operaciones:</p>
-            <ul>
-                <li>Obtener todos los usuarios: <code>getAllUsers</code></li>
-                <li>Obtener detalles de un usuario: <code>getUserDetail</code></li>
-                <li>Crear un usuario: <code>createUser</code></li>
-                <li>Actualizar un usuario: <code>updateUser</code></li>
-                <li>Eliminar un usuario: <code>deleteUser</code></li>
-            </ul>
-            <p>Puedes ver el archivo WSDL desde el siguiente enlace:</p>
-            <a href="/users/wsdl">Ver WSDL</a>
-            </body>
-        </html>
-        `);
-        }
-        );
-
-        app.get("/roles", (req, res) => {
-            res.send(`
-    <html>
-        <head>
-        <title>Servicio de Roles</title>
-        </head>
-        <body>
-        <h1>Bienvenido al Servicio de Roles</h1>
-        <p>Este servicio SOAP permite realizar las siguientes operaciones:</p>
-        <ul>
-            <li>Obtener todos los roles: <code>getRoles</code></li>
-            <li>Crear un rol: <code>createRole</code></li>
-        </ul>
-        <p>Puedes ver el archivo WSDL desde el siguiente enlace:</p>
-        <a href="/roles/wsdl">Ver WSDL</a>
-        </body>
-    </html>
-    `);
-        });
-
-        app.get("/statuses", (req, res) => {
-            res.send(`
-    <html>
-        <head>
-        <title>Servicio de Estados</title>
-        </head>
-        <body>
-        <h1>Bienvenido al Servicio de Estados</h1>
-        <p>Este servicio SOAP permite realizar las siguientes operaciones:</p>
-        <ul>
-            <li>Obtener todos los estados: <code>getStatus</code></li>
-            <li>Crear un estado: <code>createStatus</code></li>
-        </ul>
-        <p>Puedes ver el archivo WSDL desde el siguiente enlace:</p>
-        <a href="/statuses/wsdl">Ver WSDL</a>
-        </body>
-    </html>
-    `);
-        });
-
-        app.get("/login", (req, res) => {
-            res.send(`
-    <html>
-        <head>
-        <title>Servicio de Login</title>
-        </head>
-        <body>
-        <h1>Bienvenido al Servicio de Login</h1>
-        <p>Este servicio SOAP permite realizar las siguientes operaciones:</p>
-        <ul>
-            <li>Iniciar Sesión: <code>login</code></li>
-            <li>Cerrar Sesión: <code>logout</code></li>
-        </ul>
-        <p>Puedes ver el archivo WSDL desde el siguiente enlace:</p>
-        <a href="/login/wsdl">Ver WSDL</a>
-        </body>
-    </html>
-    `);
-        });
-
-        // Servir archivos WSDL
-        app.get('/statuses/wsdl', (req, res) => {
-            res.set('Content-Type', 'text/xml');
-            res.send(statusesWSDL);
-        });
-
-        app.get('/roles/wsdl', (req, res) => {
-            res.set('Content-Type', 'text/xml');
-            res.send(rolesWSDL);
-        });
-
-        app.get('/users/wsdl', (req, res) => {
-            res.set('Content-Type', 'text/xml');
-            res.send(usersWSDL);
-        });
-
-        app.get('/login/wsdl', (req, res) => {
-            res.set('Content-Type', 'text/xml');
-            res.send(loginWSDL);
-        });
-
-        // Ruta para obtener productos desde el servicio de usuarios
-        app.get('/products', async (req, res) => {
-            try {
-                const xmlResponse = await viewProductsClient(req, res);
-                res.set('Content-Type', 'text/xml'); // Establece el tipo de contenido como XML
-                return res.status(200).send(xmlResponse); // Envía la respuesta XML
-            } catch (error) {
-                console.error('Error al obtener productos:', error);
-                return res.status(500).send(`<error>Error al obtener productos: ${error.message}</error>`);
-            }
-        });
-
-        app.get('/products/search/:name', async (req, res) => {
-            try {
-                const xmlResponse = await searchProduct(req, res);
-                res.set('Content-Type', 'text/xml');
-                return res.status(200).send(xmlResponse);
-            } catch (error) {
-                console.error('Error al buscar este producto:', error);
-                return res.status(500).send(`<error>Error al buscar este producto: ${error.message}</error>`);
-            }
-        });
-
-
-        app.get('/products/filter/:category', async (req, res) => {
-            try {
-                const xmlResponse = await filterProducts(req, res);
-                res.set('Content-Type', 'text/xml');
-                return res.status(200).send(xmlResponse);
-            } catch (error) {
-                console.error('Error al obtener productos por categoría:', error);
-                return res.status(500).send(`<error>Error al obtener productos por categoría: ${error.message}</error>`);
-            }
-        });
+        app.use(routes);
 
         app.listen(port, function () {
             soap.listen(app, '/users', userService, usersWSDL);
@@ -192,5 +56,5 @@ sequelize.authenticate()
         });
     })
     .catch(err => {
-        console.log('No se puede conectar a la base de datos:', err);
+        console.log('No se puede conectar a la base de datos:\n', err);
     });
